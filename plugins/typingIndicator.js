bf61bcb831e94c748c5401f555de772b2765e769
Ian Jennings <ian@meetjennings.com>
2016-10-12 18:41:46 -0500

    syntax fixes

--- plugins/typingIndicator.js
+++ plugins/typingIndicator.js
@@ -1,22 +1,24 @@
 "use strict";
 
-const defaults = {
-    timeout: 1000
-};
+const defaults = {timeout: 1000};
 
 let plugin = function(config) {
     
     OCF.Chat.prototype.startTyping = function() {
+
         this.publish('startTyping');
         setTimeout (() => {
             this.stopTyping();
         }, config.timeout);
+
     }
 
     OCF.Chat.prototype.stopTyping = function() {
+
         this.publish('stopTyping');
+        
     }
 
 }
 
-OCF.typingIndicator = new OCF.Plugin(defaults, plugin);
+OCF.typingIndicator = new OCF.Plugin(plugin, plugin);
