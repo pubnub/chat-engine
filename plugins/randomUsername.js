6d4d062f19c6e8fe80a0270e314f47c08fcf4113
Ian Jennings <ian@meetjennings.com>
2016-10-27 16:07:04 -0500

    rnadom username plugins

---
+++ plugins/randomUsername.js
@@ -0,0 +1,42 @@
+"use strict";
+
+var randomName = function() {
+
+  var animals = ['pigeon', 'seagull', 'bat', 'owl', 'sparrows', 'robin', 'bluebird', 'cardinal', 'hawk', 'fish', 'shrimp', 'frog', 'whale', 'shark', 'eel', 'seal', 'lobster', 'octopus', 'mole', 'shrew', 'rabbit', 'chipmunk', 'armadillo', 'dog', 'cat', 'lynx', 'mouse', 'lion', 'moose', 'horse', 'deer', 'raccoon', 'zebra', 'goat', 'cow', 'pig', 'tiger', 'wolf', 'pony', 'antelope', 'buffalo', 'camel', 'donkey', 'elk', 'fox', 'monkey', 'gazelle', 'impala', 'jaguar', 'leopard', 'lemur', 'yak', 'elephant', 'giraffe', 'hippopotamus', 'rhinoceros', 'grizzlybear'];
+
+  var colors = ['silver', 'gray', 'black', 'red', 'maroon', 'olive', 'lime', 'green', 'teal', 'blue', 'navy', 'fuchsia', 'purple'];
+
+  return colors[Math.floor(Math.random() * colors.length)] + '_' + animals[Math.floor(Math.random() * animals.length)];
+
+}
+
+const defaults = {timeout: 1000};
+
+var plugin = function() {
+    
+    let Me = {
+        init: function() {
+            console.log('init called', this.parent);
+
+            if(!this.parent.state) {
+                this.parent.state = {};
+            }
+            this.parent.state.username = randomName();
+        }
+
+    }
+
+    return {
+        namespace: 'randomUsername',
+        extends: {
+            Me
+        }
+    }
+
+}
+
+if(typeof module !== "undefined") {
+    module.exports = plugin;
+} else {
+    window.randomUsername = plugin;
+}
