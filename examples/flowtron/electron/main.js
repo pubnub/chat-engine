98f74e6ab3ec1d9862819182b84884dbe9a5938e
Ian Jennings <ian@meetjennings.com>
2017-04-26 21:37:01 -0500

    desktop notification example

--- examples/flowtron/electron/main.js
+++ examples/flowtron/electron/main.js
@@ -2,6 +2,16 @@ const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
 
+const notifier = require('node-notifier');
+// String 
+notifier.notify('Message');
+ 
+// Object 
+notifier.notify({
+  'title': 'My notification',
+  'message': 'Hello, there!'
+});
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
