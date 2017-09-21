4a4855e8e50360450af9e58eaad775b75c0c13d1
Max Presman <max@pubnub.com>
2017-09-21 12:29:33 -0700

    add linting to file

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,29 @@
 const gulp = require('gulp');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
-const package = require('./package.json');
+const packageJSON = require('./package.json');
+const eslint = require('gulp-eslint');
 
 // task
-gulp.task('compile', function () {
+gulp.task('compile', () => {
 
-    browserify({
-        entries: ['./src/window.js'],
-        debug: true
-    })
-    .bundle()
-    .pipe(source('chat-engine.js'))
-    .pipe(gulp.dest('./dist/latest/'))
-    .pipe(gulp.dest('./dist/v/' + package.version));
+    browserify({ entries: ['./src/window.js'], debug: true })
+        .bundle()
+        .pipe(source('chat-engine.js'))
+        .pipe(gulp.dest('./dist/latest/'))
+        .pipe(gulp.dest('./dist/v/' + packageJSON.version));
 
 });
 
-gulp.task('default', ['compile']);
+gulp.task('lint_code', [], () => gulp.src(['src/**/*.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError()));
 
-gulp.task('watch', function() {
-  gulp.watch('./src/*', ['compile']);
+gulp.task('default', ['compile']);
+gulp.task('lint', ['lint_code']);
+gulp.task('test', ['lint']);
+
+gulp.task('watch', () => {
+    gulp.watch('./src/*', ['compile']);
 });
