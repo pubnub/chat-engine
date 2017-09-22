1dc3851e7096c96459bd1cfc5d0a74c181b522f0
Max Presman <max@pubnub.com>
2017-09-21 21:38:49 -0700

    embed testing

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const packageJSON = require('./package.json');
 const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+const runSequence = require('run-sequence');
 
 // task
 gulp.task('compile', () => {
@@ -15,14 +17,32 @@ gulp.task('compile', () => {
 
 });
 
-gulp.task('lint_code', [], () => gulp.src(['src/**/*.js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError()));
+gulp.task('lint_code', [], () => {
+    return gulp.src(['src/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError())
+});
+
+gulp.task('lint_tests', [], () => {
+    return gulp.src(['src/**/*.js'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('unit_tests', () => {
+    return gulp.src(['test/**/*.test.js'], { read: false })
+        .pipe(mocha({ reporter: 'spec' }));
+});
 
 gulp.task('default', ['compile']);
-gulp.task('lint', ['lint_code']);
-gulp.task('test', ['lint']);
+
+gulp.task('validate', ['lint_code', 'lint_tests']);
+
+gulp.task('test', (done) => {
+    runSequence('unit_tests', 'validate', done);
+});
 
 gulp.task('watch', () => {
     gulp.watch('./src/*', ['compile']);
