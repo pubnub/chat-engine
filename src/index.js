6741262e96ae9d48bccf25ab6af950f8e1ad18e6
Ian Jennings <ian@meetjennings.com>
2017-09-25 16:51:08 -0500

    begin manually merging

--- src/index.js
+++ src/index.js
@@ -8,16 +8,15 @@ Global object used to create an instance of {@link ChatEngine}.
 @param pnConfig {Object} ChatEngine is based off PubNub. Supply your PubNub configuration parameters here. See the getting started tutorial and [the PubNub docs](https://www.pubnub.com/docs/java-se-java/api-reference-configuration).
 @param ceConfig {Object} A list of chat engine specific config options.
 @param [ceConfig.globalChannel=chat-engine] {String} The root channel. See {@link ChatEngine.global}
-@param [ceConfig.authUrl] {String} The root URL used to manage permissions for private channels. Omitting this forces insecure mode.
 @param [ceConfig.throwErrors=true] {Boolean} Throws errors in JS console.
-@param [ceConfig.insecure=true] {Boolean} Force into insecure mode. Will ignore authUrl and all Chats will be public.
+@param [ceConfig.endpoint] {String} The root URL used to manage permissions for private channels.
 @return {ChatEngine} Returns an instance of {@link ChatEngine}
 @example
 ChatEngine = ChatEngineCore.create({
     publishKey: 'demo',
     subscribeKey: 'demo'
 }, {
-    authUrl: 'http://localhost/auth',
+    endpoint: 'http://localhost/auth',
     globalChannel: 'chat-engine-global-channel'
 });
 */
@@ -31,12 +30,6 @@ const create = (pnConfig, ceConfig = {}) => {
 
     if (typeof ceConfig.throwErrors === 'undefined') {
         ceConfig.throwErrors = true;
-    }
-
-    ceConfig.insecure = ceConfig.insecure || false;
-    if (!ceConfig.authUrl) {
-        console.info('ChatEngine is running in insecure mode. Supply a authUrl to run in secure mode.');
-        ceConfig.insecure = true;
     }
 
     // return an instance of ChatEngine
