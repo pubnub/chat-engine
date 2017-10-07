f383280f7b3374e31f32a2dd8bd065a3f7036904
Dustin Raimondi <raimondi1337@gmail.com>
2017-10-06 17:22:05 -0700

    half done blocks uploading

--- setup/setup.js
+++ setup/setup.js
@@ -3,21 +3,133 @@ let api = new Client({
     endpoint: 'https://admin.pubnub.com'
 });
 
-let Provision = (email, password, callback = function(){}, status = function(){}) => {
+let Provision = (email, password, callback = function(){}, status = function(){}, choice = function(){}) => {
 
-    status('Logging in...');
+    status('Logging user in...');
 
     api.init({
         email: email,
         password: password
-    }, (err, body) => {
+    }, (err, response) => {
 
         if(err) {
             return callback('Incorrect email or password. Reset your password <a href="https://admin.pubnub.com/#/forgot-password/">here</a>.');
         }
 
-        status('Creating new PubNub app...');
+        status('Getting user accounts...');
 
+        let login = response.result;
+        console.log(login.user.id);
+
+        api.request('get', ['api','accounts'], { 
+            data: { 
+                user_id: login.user.id 
+            }
+        }, function (err, response) {
+            if(err) {
+                return callback('Could not get PubNub accounts. Please contact support@pubnub.com.');
+            }
+
+            let account = response.result.accounts[0];
+            console.log(account.id);
+
+            status('Using account ' + account.properties.company + ', if this is incorrent deploy manually or log in as another user');
+            status('Creating new PubNub app...');
+
+            api.request('post', ['api','apps'], {
+                data:{
+                    name: 'ChatEngine App',
+                    owner_id: account.id,
+                    properties: {}
+                }
+            }, function (err, response) {
+
+                if(err) {
+                    return callback('Could not create new PubNub app. Please contact support@pubnub.com.');
+                }
+
+                let app = response.result;
+                console.log(app);
+
+                status('Getting PubNub keys...');
+
+                api.request('get',['api','apps'], {
+                    data: {
+                        owner_id: account.id
+                    }
+                }, function (err, response) {
+
+                    if(err) {
+                        return callback('Could not get PubNub keys. Please contact support@pubnub.com.');
+                    }
+
+                    let apps = response.result;
+                    let key;
+
+                    for (item of apps){
+                        if (item.id == app.id){
+                            key = item.keys[0]
+                        }
+                    }
+
+                    console.log(key);
+
+                    status('Enabling PubNub features...');
+
+                    key.properties.name = 'ChatEngine Keyset';
+                    key.properties.presence = 1;
+                    key.properties.history = 1;
+                    key.properties.message_storage_ttl = 7;
+                    key.properties.multiplexing = 1;
+                    key.properties.presence_announce_max = 20;
+                    key.properties.presence_debounce = 2;
+                    key.properties.presence_global_here_now = 1;
+                    key.properties.presence_interval = 10;
+                    key.properties.presence_leave_on_disconnect = 0;
+                    key.properties.blocks = 1;
+                    key.properties.uls = 1;
+                    key.properties.wildcardsubscribe = 1;
+
+                    api.request('put', ['api', 'keys', key.id], {
+                        data: key 
+                    }, function (err, response) {
+
+                        if(err) {
+                            callback('Could not enable PubNub features. Please contact support@pubnub.com.')
+                        }
+
+                        status('Creating new PubNub Function...');
+
+                        api.request('post',['api','v1','blocks','key',key.id,'block'], {
+                            data: {
+                                name:'ChatEngine Function',
+                                key_id: key.id
+                            }
+                        }, function (err, response) {
+                            if(err){
+                                callback('Could not create new PubNub Function. Please contact support@pubnub.com.')
+                            }
+                            let block = response.payload;
+                            
+                            status('Creating new Event Handlers...');
+
+                            api.request('post',['api','v1','blocks','key',key.id,'event_handler'], {
+                                data: {
+                                    key_id: key.id,
+                                    block_id: block.id,
+                                    type: 'js',
+                                    event: 'js-after-publish',
+                                    channel: 'global',
+                                    
+                                }
+                        });
+                    });
+                });
+            });
+        });
+    });
+}
+        /*
         let session = body.result;
 
          api.request('post', ['api', 'apps'], {
@@ -33,7 +145,7 @@ let Provision = (email, password, callback = function(){}, status = function(){}
             }
 
             status('Creating new PubNub keys...');
-
+            
              api.request('get', ['api', 'apps'], {
                 data: {
                     owner_id: session.user.id,
@@ -90,4 +202,4 @@ let Provision = (email, password, callback = function(){}, status = function(){}
 
     });
 
-}
+}*/
