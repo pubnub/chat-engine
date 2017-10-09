4320c6ed113180fe708cc9b03fb81920655621d5
Dustin Raimondi <raimondi1337@gmail.com>
2017-10-09 13:14:15 -0700

    codacy

--- setup/setup.js
+++ setup/setup.js
@@ -3,7 +3,7 @@ let api = new Client({
     endpoint: 'https://admin.pubnub.com'
 });
 
-let Provision = (email, password, callback = function() {}, status = function() {}) => {
+let Provision = (email, password, callback = function () {}, status = function () {}) => {
 
     status('Logging user in...');
 
@@ -24,7 +24,7 @@ let Provision = (email, password, callback = function() {}, status = function() 
             data: {
                 user_id: login.user.id
             }
-        }, function(err, response) {
+        }, (err, response) => {
 
             if (err) {
                 return callback('Could not get PubNub accounts. Please contact support@pubnub.com.');
@@ -41,7 +41,7 @@ let Provision = (email, password, callback = function() {}, status = function() 
                     owner_id: account.id,
                     properties: {}
                 }
-            }, function(err, response) {
+            }, (err, response) => {
 
                 if (err) {
                     return callback('Could not create new PubNub app. Please contact support@pubnub.com.');
@@ -55,7 +55,7 @@ let Provision = (email, password, callback = function() {}, status = function() 
                     data: {
                         owner_id: account.id
                     }
-                }, function(err, response) {
+                }, (err, response) => {
 
                     if (err) {
                         return callback('Could not get PubNub keys. Please contact support@pubnub.com.');
@@ -64,9 +64,9 @@ let Provision = (email, password, callback = function() {}, status = function() 
                     let apps = response.result;
                     let key;
 
-                    for (item of apps) {
+                    for (let item of apps) {
                         if (item.id == app.id) {
-                            key = item.keys[0]
+                            key = item.keys[0];
                         }
                     }
 
@@ -88,7 +88,7 @@ let Provision = (email, password, callback = function() {}, status = function() 
 
                     api.request('put', ['api', 'keys', key.id], {
                         data: key
-                    }, function(err, response) {
+                    }, (err, response) => {
 
                         if (err) {
                             callback('Could not enable PubNub features. Please contact support@pubnub.com.');
@@ -101,7 +101,7 @@ let Provision = (email, password, callback = function() {}, status = function() 
                                 name: 'ChatEngine Function',
                                 key_id: key.id
                             }
-                        }, function(err, response) {
+                        }, (err, response) => {
 
                             if (err) {
                                 callback('Could not create new PubNub Function. Please contact support@pubnub.com.');
@@ -111,7 +111,7 @@ let Provision = (email, password, callback = function() {}, status = function() 
 
                             status('Creating new after-presence Event Handler...');
 
-                            $.get('functions/state-to-kv.js', function(code) {
+                            $.get('functions/state-to-kv.js', (code) => {
 
                                 api.request('post', ['api', 'v1', 'blocks', 'key', key.id, 'event_handler'], {
                                     data: {
@@ -124,7 +124,7 @@ let Provision = (email, password, callback = function() {}, status = function() 
                                         code: code,
                                         output: 'output-0.5823105682419438'
                                     }
-                                }, function(err, response) {
+                                }, (err, response) => {
 
                                     if (err) {
                                         callback('Could not create new PubNub after-publish Event Handler. Please contact support@pubnub.com.');
@@ -132,7 +132,7 @@ let Provision = (email, password, callback = function() {}, status = function() 
 
                                     status('Creating new after-publish Event Handler...');
 
-                                    $.get('functions/get-kv-state.js', function(code) {
+                                    $.get('functions/get-kv-state.js', (code) => {
 
                                         api.request('post', ['api', 'v1', 'blocks', 'key', key.id, 'event_handler'], {
                                             data: {
@@ -145,7 +145,7 @@ let Provision = (email, password, callback = function() {}, status = function() 
                                                 code: code,
                                                 output: 'output-0.5823105682419438'
                                             }
-                                        }, function(err, response) {
+                                        }, (err, response) => {
 
                                             if (err) {
                                                 callback('Could not create new PubNub after-publish Event Handler. Please contact support@pubnub.com.');
@@ -159,7 +159,7 @@ let Provision = (email, password, callback = function() {}, status = function() 
                                                     key_id: key.id,
                                                     action: 'start'
                                                 }
-                                            }, function(err, response) {
+                                            }, (err, response) => {
 
                                                 if (err) {
                                                     callback('Could not start PubNub Function. Please contact support@pubnub.com.');
@@ -180,77 +180,3 @@ let Provision = (email, password, callback = function() {}, status = function() 
         });
     });
 }
-/*
-        let session = body.result;
-
-         api.request('post', ['api', 'apps'], {
-            data: {
-                name: 'ChatEngine App',
-                owner_id: session.user.id,
-                properties: {}
-            }
-        }, function (err, created) {
-
-            if(err) {
-                return callback('Could not create new PubNub app. Please contact support@pubnub.com.');
-            }
-
-            status('Creating new PubNub keys...');
-            
-             api.request('get', ['api', 'apps'], {
-                data: {
-                    owner_id: session.user.id,
-                }
-            }, function (err, data) {
-
-                if(err) {
-                    return callback('Could not fetch PubNub apps. Please contact support@pubnub.com.');
-                }
-
-                status('Enabling PubNub features...');
-
-                let result = false;
-
-                data.result.forEach((res) => {
-                    if(res.id == created.result.id) {
-                        result = res;
-                    }
-                });
-
-                let key = result.keys[0];
-
-                key.properties.name = 'ChatEngine Keyset';
-                key.properties.presence = 1;
-                key.properties.history = 1;
-                key.properties.message_storage_ttl = 7;
-                key.properties.multiplexing = 1;
-                key.properties.presence_announce_max = 20;
-                key.properties.presence_debounce = 2;
-                key.properties.presence_global_here_now = 1;
-                key.properties.presence_interval = 10;
-                key.properties.presence_leave_on_disconnect = 0;
-                key.properties.blocks = 1;
-                key.properties.uls = 1;
-                key.properties.wildcardsubscribe = 1;
-
-                api.request('put', ['api', 'keys', key.id], {
-                    data: key
-                }, function (err, data) {
-
-                    if(err) {
-                        return callback('Could not create a new key. Please contact support@pubnub.com.');
-                    }
-
-                    status('Done!');
-
-                    callback(null, data);
-
-                });
-
-            });
-
-        });
-
-    });
-
-}*/
