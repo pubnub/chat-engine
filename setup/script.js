2b36d45c896fa5d3bfda2c3e57901d93720c6b9d
Dustin Raimondi <raimondi1337@gmail.com>
2017-10-30 13:01:22 -0700

    formatting

--- setup/script.js
+++ setup/script.js
@@ -9,46 +9,46 @@ const emailElement = $('#email');
 const passwordElement = $('#password');
 
 provisionElement.submit(() => {
-  provisionElement.hide();
-  loadElement.show();
-  errorElement.hide();
-  statusElement.empty();
+    provisionElement.hide();
+    loadElement.show();
+    errorElement.hide();
+    statusElement.empty();
 
-  Provision(emailElement.val(), passwordElement.val(), function(err, data) {
+    Provision(emailElement.val(), passwordElement.val(), function(err, data) {
 
-    if(err) {
+        if (err) {
 
-      loadElement.hide();
-      provisionElement.show();
+            loadElement.hide();
+            provisionElement.show();
 
-      errorOutElement.html(err);
-      errorElement.show();
+            errorOutElement.html(err);
+            errorElement.show();
 
-    } else {
+        } else {
 
-      loadElement.hide();
+            loadElement.hide();
 
-      let output = "";
-      output += "// Make sure to import ChatEngine first!\n";
-      output += "ChatEngine = ChatEngineCore.create({\n";
-      output += "    publishKey: '" + data.pub + "',\n";
-      output += "    subscribeKey: '" + data.sub + "'\n";
-      output += "}, {\n";
-      output += "    globalChannel: 'global',\n";
-      output += "    endpoint: 'https://pubsub.pubnub.com/v1/blocks/sub-key/" + data.sub + "/chat-engine-server'\n";
-      output += "});\n";
+            let output = "";
+            output += "// Make sure to import ChatEngine first!\n";
+            output += "ChatEngine = ChatEngineCore.create({\n";
+            output += "    publishKey: '" + data.pub + "',\n";
+            output += "    subscribeKey: '" + data.sub + "'\n";
+            output += "}, {\n";
+            output += "    globalChannel: 'global',\n";
+            output += "    endpoint: 'https://pubsub.pubnub.com/v1/blocks/sub-key/" + data.sub + "/chat-engine-server'\n";
+            output += "});\n";
 
-      codeElement.text(output);
-      outputElement.show();
+            codeElement.text(output);
+            outputElement.show();
 
-    }
+        }
 
-  }, function(statusText){
+    }, function(statusText) {
 
-    statusElement.append($('<li class="list-group-item">' + statusText + '</li>'));
+        statusElement.append($('<li class="list-group-item">' + statusText + '</li>'));
 
-  });
+    });
 
-  return false;
+    return false;
 
 });
