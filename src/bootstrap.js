48ac389d15c9ed714fed91f1f802b8d1a40726e0
Ian Jennings <ian@meetjennings.com>
2017-11-03 12:04:43 -0500

    small endpoints

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -369,7 +369,7 @@ module.exports = (ceConfig, pnConfig) => {
                         authKey: pnConfig.authKey
                     }, {
                         params: {
-                            route: 'group'
+                            route: 'group_permissions'
                         }
                     }).then((response) => {
 
