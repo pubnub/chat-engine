91fbbbe8110e68f9b803c2ad130de08ad74d6914
Manuel Fernando <manuelcamaron@gmail.com>
2017-11-03 12:16:31 -0500

    add capability to manage states (#192)
    * add capability to manage states
    
    * bind state functions directly to the plugin
    
    * add a test for plugin state storage
    
    * add exit criteria test
    
    * isolate dataset for plugin's namespace
    
    * Scoped state in plugin

--- src/modules/emitter.js
+++ src/modules/emitter.js
@@ -26,6 +26,12 @@ class Emitter extends RootEmitter {
         this.plugins = [];
 
         /**
+         Stores in memory keys and values
+         @private
+         */
+        this._dataset = {};
+
+        /**
          Emit events locally.
 
          @private
@@ -77,15 +83,34 @@ class Emitter extends RootEmitter {
 
     // add an object as a subobject under a namespoace
     addChild(childName, childOb) {
-
         // assign the new child object as a property of parent under the
         // given namespace
         this[childName] = childOb;
+
+        // assign a data set for the namespace if it doesn't exist
+        if (!this._dataset[childName]) {
+            this._dataset[childName] = {};
+        }
 
         // the new object can use ```this.parent``` to access
         // the root class
         childOb.parent = this;
 
+        // bind get() and set() to the data set
+        childOb.get = this.get.bind(this._dataset[childName]);
+        childOb.set = this.set.bind(this._dataset[childName]);
+    }
+
+    get(key) {
+        return this[key];
+    }
+
+    set(key, value) {
+        if (this[key] && !value) {
+            delete this[key];
+        } else {
+            this[key] = value;
+        }
     }
 
     /**
@@ -100,7 +125,6 @@ class Emitter extends RootEmitter {
 
         // see if there are plugins to attach to this class
         if (module.extends && module.extends[this.name]) {
-
             // attach the plugins to this class
             // under their namespace
             this.addChild(module.namespace, new module.extends[this.name]());
