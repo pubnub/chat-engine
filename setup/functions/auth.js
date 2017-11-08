642e2fa1f3d0052745e7f5be1c82c0c339cab1b8
Ian Jennings <ian@meetjennings.com>
2017-11-07 22:08:54 -0800

    fix auth endpoint

--- setup/functions/auth.js
+++ setup/functions/auth.js
@@ -11,92 +11,92 @@ export default (request, response) => {
     let proxyParams = proxyRequest.params;
 
     let isAuthed = (record, who) => {
-        return who && record && record.length > 0 && record.indexOf(who) > -1;
+
+        if (!record) {
+            return true;
+        } else {
+            return who && record && record.length > 0 && record.indexOf(who) > -1;
+        }
+
+    };
+
+    let allow = () => {
+        response.status = 200;
+        return response.send();
+    };
+
+    let reject = () => {
+        response.status = 401;
+        return response.send();
+    };
+
+    let die = (error) => {
+        response.status = 500;
+        return response.send(error);
     };
 
     let authInChannel = (record, who) => {
 
+        record = record || [];
+
         let key = ['authed', proxyBody.chat.channel].join(':');
 
-        record.push(who);
-        return db.set(key, record, 525600);
-    };
+        if (record.indexOf(who) === -1) {
 
+            record.push(who);
+
+            return db.set(key, record, 525600).then(() => {
+                return allow();
+            }).catch((err) => {
+                return die(err);
+            });
+
+        } else {
+            return allow();
+        }
+
+    };
 
     if (proxyParams.route === 'invite') {
 
         if (!proxyBody.chat.private) {
             response.status = 200;
             return response.send();
-        }
-
-
-        let key = ['authed', proxyBody.chat.channel].join(':');
-        db.get(key).then((record) => {
-
-            if (isAuthed(record, proxyBody.uuid)) {
-
-                return authInChannel(record, proxyBody.to).then(() => {
-                    response.status = 200;
-                    return response.send();
-                }).catch(() => {
-                    response.status = 500;
-                    return response.send();
-                });
-
-            } else {
-
-                response.status = 401;
-                return response.send();
-
-            }
-        }).catch(() => {
-            response.status = 500;
-            return response.send();
-        });
-
-    } else if (proxyParams.route === 'grant') {
-
-        if (!proxyBody.chat.private) {
-
-            response.status = 200;
-            return response.send();
         } else {
 
             let key = ['authed', proxyBody.chat.channel].join(':');
-            db.get(key).then((record) => {
+            return db.get(key).then((record) => {
 
                 if (isAuthed(record, proxyBody.uuid)) {
-
-                    response.status = 200;
-                    return response.send();
-
+                    return authInChannel(record, proxyBody.to);
                 } else {
-
-                    response.status = 401;
-                    return response.send();
+                    return reject();
                 }
-
-            }).catch(() => {
-
-                return authInChannel([], proxyBody.uuid).then(() => {
-                    response.status = 200;
-                    return response.send();
-                }).catch(() => {
-                    response.status = 500;
-                    return response.send();
-                });
-
+            }).catch((err) => {
+                return die(err);
             });
 
         }
 
-    }
+    } else if (proxyParams.route === 'grant') {
 
-    return request.json().then((body) => {
-        return response.send(body);
-    }).catch(() => {
-        return response.send('Malformed JSON body');
-    });
+        if (proxyBody.chat.private) {
+            let key = ['authed', proxyBody.chat.channel].join(':');
+            return db.get(key).then((record) => {
+                if (isAuthed(record, proxyBody.uuid)) {
+                    return authInChannel(record, proxyBody.uuid);
+                } else {
+                    return reject();
+                }
+            }).catch((err) => {
+                return die(err);
+            });
+        } else {
+            return allow();
+        }
+
+    } else {
+        return allow();
+    }
 
 };
