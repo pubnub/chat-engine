1eac30ce09547c69535548c655aa5feeda9b736d
Max Presman <max@presman.ca>
2017-11-07 16:24:35 -0800

    fix up setup blocks (#201)
    * fix up setup blocks
    
    * remove ECMA6 code

--- setup/setup_blocks.js
+++ setup/setup_blocks.js
@@ -1,125 +1,136 @@
-let ProvisionBlocks = (api, userId, key, callback = function() {}, status = function() {}) => {
+var ProvisionBlocks = function (api, userId, key, callback = function() {}, status = function() {}) {
+
+  var block = null;
+
   status('Creating new PubNub Function...');
 
-  api.request('post', ['api', 'v1', 'blocks', 'key', key.id, 'block'], {
-    data: {
-      name: 'ChatEngine Function',
-      key_id: key.id
-    }
-  }, (err, response) => {
+  var addSecretKeyToVault = function() {
+    status('Adding Secret Key to Functions Vault...');
 
-    if (err) {
-      callback('Could not create new PubNub Function. Please contact support@pubnub.com.');
-    }
+    api.request('put', ['api', 'vault', key.subscribe_key, 'key', 'secretKey'], {
+      contentType: 'application/json',
+      data: JSON.stringify({
+        keyName: 'secretKey',
+        key_id: key.id,
+        subscribeKey: key.subscribe_key,
+        value: key.secret_key
+      })
+    }, function (err, response) {
 
-    let block = response.payload;
+      if (err) {
+        callback('Could not add Secret Key to Functions Vault. Please contact support@pubnub.com');
+        return;
+      }
 
-    status('Creating new after-presence Event Handler...');
+      status('Success!');
 
-    $.get('functions/state-to-kv.js', (code) => {
+      callback(null, {
+        pub: key.publish_key,
+        sub: key.subscribe_key
+      });
+    });
+  }
+
+  var startPubNubFunction = function() {
+    status('Starting Pubnub Function...');
+
+    api.request('post', ['api', 'v1', 'blocks', 'key', key.id, 'block', block.id, 'start'], {
+      data: {
+        block_id: block.id,
+        key_id: key.id,
+        action: 'start'
+      }
+    }, function (err, response) {
+      if (err) {
+        callback('Could not start PubNub Function. Please contact support@pubnub.com.');
+        return;
+      }
+
+      addSecretKeyToVault();
+    });
+  }
+
+  var onCodeFetch = function(stateCodeResult, authCodeResult, functionCodeResult) {
+    status('Creating new after-publish Event Handler...');
+
+    api.request('post', ['api', 'v1', 'blocks', 'key', key.id, 'event_handler'], {
+      data: {
+        key_id: key.id,
+        block_id: block.id,
+        type: 'js',
+        event: 'js-after-presence',
+        channels: 'global',
+        name: 'state-to-kv',
+        code: stateCodeResult[0],
+        output: 'output-state-to-kv-' + (new Date()).getTime()
+      }
+    }, function (err, response) {
+      if (err) {
+        callback('Could not create new PubNub after-publish Event Handler. Please contact support@pubnub.com.');
+      }
 
       api.request('post', ['api', 'v1', 'blocks', 'key', key.id, 'event_handler'], {
         data: {
           key_id: key.id,
           block_id: block.id,
           type: 'js',
-          event: 'js-after-presence',
-          channels: '*',
-          name: 'chat-engine-state',
-          code: code,
-          output: 'output-state-to-kv-' + (new Date()).getTime()
+          event: 'js-on-rest',
+          path: 'chat-engine-auth',
+          name: 'chat-engine-auth',
+          code: authCodeResult[0],
+          output: 'auth-' + Math.round((new Date()).getTime())
         }
-      }, (err, response) => {
-
+      }, function (err, response) {
         if (err) {
           callback('Could not create new PubNub after-publish Event Handler. Please contact support@pubnub.com.');
         }
 
-        status('Creating new after-publish Event Handler...');
+        status('Creating new on-request Event Handler...');
 
-        $.get('functions/auth.js', (code) => {
+        api.request('post', ['api', 'v1', 'blocks', 'key', key.id, 'event_handler'], {
+          data: {
+            key_id: key.id,
+            block_id: block.id,
+            code: functionCodeResult[0],
+            type: 'js',
+            name: 'chat-engine-server',
+            path: 'server',
+            event: 'js-on-rest',
+            output: 'output-server-endpoint-' + Math.round((new Date()).getTime())
+          }
+        }, function (err, response) {
+          if (err) {
+            callback('Could not create new Pubnub on-request Event Handler. Please contact support@pubnub.com.');
+            return;
+          }
 
-          api.request('post', ['api', 'v1', 'blocks', 'key', key.id, 'event_handler'], {
-            data: {
-              key_id: key.id,
-              block_id: block.id,
-              type: 'js',
-              event: 'js-on-rest',
-              path: 'chat-engine-auth',
-              name: 'chat-engine-auth',
-              code: code,
-              output: 'auth-' + Math.round((new Date()).getTime())
-            }
-          }, (err, response) => {
-
-            if (err) {
-              callback('Could not create new PubNub after-publish Event Handler. Please contact support@pubnub.com.');
-            }
-
-            status('Creating new on-request Event Handler...');
-
-            $.get('functions/server.js', (code) => {
-
-              api.request('post', ['api', 'v1', 'blocks', 'key', key.id, 'event_handler'], {
-                data: {
-                  key_id: key.id,
-                  block_id: block.id,
-                  code: code,
-                  type: 'js',
-                  name: 'chat-engine-server',
-                  path: 'chat-engine-server',
-                  event: 'js-on-rest',
-                  output: 'output-server-endpoint-' + Math.round((new Date()).getTime())
-                }
-              }, (err, response) => {
-
-                if (err) {
-                  callback('Could not create new Pubnub on-request Event Handler. Please contact support@pubnub.com.');
-                }
-
-                status('Starting Pubnub Function...');
-
-                api.request('post', ['api', 'v1', 'blocks', 'key', key.id, 'block', block.id, 'start'], {
-                  data: {
-                    block_id: block.id,
-                    key_id: key.id,
-                    action: 'start'
-                  }
-                }, (err, response) => {
-
-                  if (err) {
-                    callback('Could not start PubNub Function. Please contact support@pubnub.com.');
-                  }
-
-                  status('Adding Secret Key to Functions Vault...');
-
-                  api.request('put', ['api', 'vault', key.subscribe_key, 'key', 'secretKey'], {
-                    contentType: 'application/json',
-                    data: JSON.stringify({
-                      keyName: 'secretKey',
-                      key_id: key.id,
-                      subscribeKey: key.subscribe_key,
-                      value: key.secret_key
-                    })
-                  }, (err, response) => {
-
-                    if (err) {
-                      callback('Could not add Secret Key to Functions Vault. Please contact support@pubnub.com');
-                    }
-
-                    status('Success!');
-
-                    callback(null, {
-                      pub: key.publish_key,
-                      sub: key.subscribe_key
-                    });
-                  });
-                });
-              });
-            }, 'text');
-          });
-        }, 'text');
+          startPubNubFunction();
+        });
       });
-    }, 'text');
+    });
+  };
+
+  api.request('post', ['api', 'v1', 'blocks', 'key', key.id, 'block'], {
+    data: {
+      name: 'ChatEngine Function',
+      key_id: key.id
+    }
+  }, function (err, response) {
+
+    if (err) {
+      callback('Could not create new PubNub Function. Please contact support@pubnub.com.');
+    }
+
+    block = response.payload;
+
+    var stateCodeFetch = $.get({ url: 'functions/state-to-kv.js', dataType: 'text' });
+    var authCodeFetch = $.get({ url: 'functions/auth.js', dataType: 'text' });
+    var functionsCodeFetch = $.get({ url: 'functions/server.js', dataType: 'text' });
+
+    $.when(stateCodeFetch, authCodeFetch, functionsCodeFetch)
+      .then(onCodeFetch)
+      .catch(function (err) {
+        status('Failed to fetch code');
+      });
   });
 }
