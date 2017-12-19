d86de67b22d350330b1ada8a5b5a453897fbae51
Ian Jennings <ian@meetjennings.com>
2017-12-19 17:06:19 -0600

    refactor shell script

--- staging.sh
+++ staging.sh
@@ -3,10 +3,10 @@
 setup_git() {
   git config --global user.email "github-ops-codacy@pubnub.com"
   git config --global user.name "PubNub Bot"
+  git remote add origin-travis https://${GH_TOKEN}@github.com/pubnub/chat-engine.git > /dev/null 2>&1
 }
 
 commit_website_files() {
-  git remote add origin-travis https://${GH_TOKEN}@github.com/pubnub/chat-engine.git > /dev/null 2>&1
   git checkout staging
   git pull origin-travis staging
   git checkout master
@@ -18,12 +18,8 @@ commit_website_files() {
   git add .
   git add -f dist
   git commit --message "Build: $TRAVIS_BUILD_NUMBER"
-}
-
-upload_files() {
   git push --quiet --set-upstream origin-travis staging
 }
 
 setup_git
 commit_website_files
-upload_files
