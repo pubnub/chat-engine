60e1b94cfbb22a56beeabbf0469059cc5899ef3a
Ian Jennings <ian@meetjennings.com>
2017-12-19 17:03:39 -0600

    add remote location to script

--- staging.sh
+++ staging.sh
@@ -6,10 +6,11 @@ setup_git() {
 }
 
 commit_website_files() {
+  git remote add origin-travis https://${GH_TOKEN}@github.com/pubnub/chat-engine.git > /dev/null 2>&1
   git checkout staging
-  git pull
+  git pull origin-travis staging
   git checkout master
-  git pull
+  git pull origin-travis master
   git merge staging -s ours -m "Update staging"
   git checkout -b staging
   git merge master
@@ -20,7 +21,6 @@ commit_website_files() {
 }
 
 upload_files() {
-  git remote add origin-travis https://${GH_TOKEN}@github.com/pubnub/chat-engine.git > /dev/null 2>&1
   git push --quiet --set-upstream origin-travis staging
 }
 
