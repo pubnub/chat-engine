7e0d3dc684d25f4a0235b7d6692b26328215c605
Ian Jennings <ian@meetjennings.com>
2018-01-30 20:33:10 -0600

    attempt with new keepalive settings

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -25,6 +25,13 @@ module.exports = (ceConfig = {}, pnConfig = {}) => {
     ChatEngine.pnConfig.presenceTimeout = ChatEngine.pnConfig.presenceTimeout || 60;
     ChatEngine.pnConfig.ssl = true;
 
+    ChatEngine.pnConfig.keepAlive = true;
+    ChatEngine.pnConfig.keepAliveSettings = {
+        keepAlive: true,
+        keepAliveMsecs: 5 * 1000 * 60,
+        timeout: 5 * 1000 * 60
+    };
+
     ChatEngine.ceConfig.endpoint = ChatEngine.ceConfig.endpoint || 'https://pubsub.pubnub.com/v1/blocks/sub-key/' + ChatEngine.pnConfig.subscribeKey + '/chat-engine-server';
     ChatEngine.ceConfig.globalChannel = ChatEngine.ceConfig.globalChannel || 'chat-engine-global';
 
