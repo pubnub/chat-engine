eed136adc0a32fec8236d588f9b39ca820598a72
Ian Jennings <ian@meetjennings.com>
2018-02-23 12:57:39 -0800

    stop throwing errors for history connection

--- src/components/chat.js
+++ src/components/chat.js
@@ -603,13 +603,7 @@ class Chat extends Emitter {
     });
      */
     search(config) {
-
-        if (this.hasConnected) {
-            return new Search(this.chatEngine, this, config);
-        } else {
-            this.chatEngine.throwError(this, 'trigger', 'search', new Error('You must wait for the $.connected event before calling Chat#search'));
-        }
-
+        return new Search(this.chatEngine, this, config);
     }
 
     /**
