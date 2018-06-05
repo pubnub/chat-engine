7472a11ee79523711320689266a936774a2834cb
Ian Jennings <ian@meetjennings.com>
2018-06-04 19:18:21 -0500

    emit to chat without connecting to it first

--- test/integration/main.test.js
+++ test/integration/main.test.js
@@ -664,7 +664,7 @@ describe('remote chat list', () => {
 
 });
 
-describe('invite', () => {
+describe('interactions', () => {
 
     beforeEach(reset);
     beforeEach(createChatEngine);
@@ -718,6 +718,32 @@ describe('invite', () => {
             }
 
         });
+
+    });
+
+    it('should emit to chat without connecting', function shouldEmitNoConnect(done) {
+
+        this.timeout(60000);
+
+        let ourChan = 'chat-tester4' + new Date().getTime();
+
+        let chatConnected = new ChatEngine.Chat(ourChan);
+        let chatNOTConnected = new ChatEngineYou.Chat(ourChan, false, false);
+
+        chatNOTConnected.on('$.connected', () => {
+            done('should not connect');
+        });
+
+        chatConnected.once('something', (payload) => {
+            assert.isObject(payload);
+            done();
+        });
+
+        setTimeout(() => {
+            chatNOTConnected.emit('something', {
+                text: 'hello world'
+            });
+        }, 5000);
 
     });
 
