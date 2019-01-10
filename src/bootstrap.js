9da1c7fb1c3f77a34c4abf1ae494b87ff11765ee
Ian Jennings <ian@meetjennings.com>
2019-01-09 23:28:54 -0600

    patch

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -403,7 +403,6 @@ module.exports = (ceConfig = {}, pnConfig = {}) => {
                 /**
                  *  Fired when ChatEngine is connected to the internet and ready to go!
                  * @event ChatEngine#$"."ready
-                 * @type {object}
                  * @property {Me} me The ChatEngine {@link Me} object representing this client.
                  * @example
                  * ChatEngine.on('$.ready', (data) => {
