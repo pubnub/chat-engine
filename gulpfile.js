64de048016c622ff537f40ae5482708e13745742
Craig Baumgarten <crgbaumgart@gmail.com>
2019-01-22 14:37:50 -0800

    Added 'exit': true flag to stop hanging connections in mocha tests. (#435)
    * Added 'exit': true flag to stop hanging connections in mocha tests.
    
    * Fix comma space formatting issue.

--- gulpfile.js
+++ gulpfile.js
@@ -64,7 +64,7 @@ gulp.task('lint_tests', () => {
 
 gulp.task('run_tests', () => {
     return gulp.src(testFiles, { read: false })
-        .pipe(mocha({ reporter: 'spec' }))
+        .pipe(mocha({ reporter: 'spec', exit: true }))
         .pipe(istanbul.writeReports());
 });
 
