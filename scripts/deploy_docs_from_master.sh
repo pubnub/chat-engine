6bd90c6a9a6a189ae7c7cdbc75ad3d8fdaea3659
Craig Baumgarten <crgbaumgart@gmail.com>
2019-04-23 14:09:13 -0700

    Add travis branch as workaround.

--- scripts/deploy_docs_from_master.sh
+++ scripts/deploy_docs_from_master.sh
@@ -8,7 +8,7 @@ function uploadToGithubPagesRepo() {
   git config user.name "${GH_USER_NAME}"
   git config user.email "${GH_USER_EMAIL}"
   git commit -m "Updating ChatEngine Docs on gh-pages: ${TRAVIS_COMMIT_MESSAGE}"
-  git push --force "https://${GH_TOKEN_PUBLISH_DOCS}@github.com/pubnub/chat-engine.git" master:gh-pages
+  git push --force --quiet "https://${GH_TOKEN_PUBLISH_DOCS}@github.com/pubnub/chat-engine.git" master:gh-pages
   return
 }
 
