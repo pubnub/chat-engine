60992fadefcf11b32890170f10de92e7dd55bb29
Craig Baumgarten <crgbaumgart@gmail.com>
2019-04-25 13:36:49 -0700

    Tool/ce docs deploy to gh-pages branch (#447)
    * Add CE docs deploy logic to gh-page branch.
    
    * Always run 'gulp compile_doc' cmd.
    
    * Rm comment.
    
    * Add travis branch as workaround.

---
+++ scripts/deploy_docs_from_master.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+set -x
+
+function uploadToGithubPagesRepo() {
+  git init
+  git add .
+  git config user.name "${GH_USER_NAME}"
+  git config user.email "${GH_USER_EMAIL}"
+  git commit -m "Updating ChatEngine Docs on gh-pages: ${TRAVIS_COMMIT_MESSAGE}"
+  git push --force --quiet "https://${GH_TOKEN_PUBLISH_DOCS}@github.com/pubnub/chat-engine.git" master:gh-pages
+  return
+}
+
+## RUN
+gulp compile_docs
+if [ "${TRAVIS_BRANCH}" == "master" ] \
+  && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
+    # Build the JS_DOCs into docs/ dir
+    pushd docs
+    uploadToGithubPagesRepo()
+    popd
+fi
