12ea04a0d2455ee6f54a7cdec516b94963b4fac1
Craig Baumgarten <craigb@pubnub.com>
2019-04-25 17:30:18 -0700

    Tool/ce docs deploy to gh pages (#448)
    * Add CE docs deploy logic to gh-page branch.
    
    * Always run 'gulp compile_doc' cmd.
    
    * Rm comment.
    
    * Add travis branch as workaround.
    
    * Add image assets to guide/static files dir.
    
    * Add plugin repos to build process. First attempt.
    
    * Use CraigB. github user for pulling all plugin repos.
    
    * Refactor adding CraigB gh user.
    
    * Trying again...
    
    * Add return statements to functions
    
    * Force dir check to be true.

--- scripts/deploy_docs_from_master.sh
+++ scripts/deploy_docs_from_master.sh
@@ -12,12 +12,45 @@ function uploadToGithubPagesRepo() {
   return
 }
 
+function cloneGitRepo() {
+  REPO_TO_CLONE="${1}"
+  if [ REPO_TO_CLONE != "" ]; then
+    git clone "https://${GH_TOKEN_PUBLISH_DOCS}@github.com/pubnub/${REPO_TO_CLONE}.git"
+  fi
+  return
+}
+
+function cloneAllRepos() {
+  git config user.name "${GH_USER_NAME}"
+  git config user.email "${GH_USER_EMAIL}"
+  pwd
+  pushd ../
+
+  cloneGitRepo "chat-engine-emoji"
+  cloneGitRepo "chat-engine-gravatar"
+  cloneGitRepo "chat-engine-markdown"
+  cloneGitRepo "chat-engine-muter"
+  cloneGitRepo "chat-engine-online-user-search"
+  cloneGitRepo "chat-engine-open-graph"
+  cloneGitRepo "chat-engine-uploadcare"
+  cloneGitRepo "chat-engine-typing-indicator"
+  cloneGitRepo "chat-engine-unread-messages"
+
+  ls | grep "chat-engine" || true
+  popd
+  pwd
+  ls
+  return
+}
+
 ## RUN
+cloneAllRepos
+echo "compiling js_docs with plugin assets..."
 gulp compile_docs
 if [ "${TRAVIS_BRANCH}" == "master" ] \
   && [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     # Build the JS_DOCs into docs/ dir
     pushd docs
-    uploadToGithubPagesRepo()
+    uploadToGithubPagesRepo
     popd
 fi
